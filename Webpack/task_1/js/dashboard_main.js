e84b3e4a9320001a56ca0f44823be47976b4b29c
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-02 13:56:10 -0500

    chmod

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,12 +1,9 @@
-// Import jQuery
 import $ from 'jquery';
 
-// Wait for the document to be ready
 $(document).ready(function() {
-    // Add elements to the body
-    $('body').append('<p>Holberton Dashboard</p>'); // Paragraph element: Holberton Dashboard
-    $('body').append('<p>Dashboard data for the students</p>'); // Paragraph element: Dashboard data for the students
-    $('body').append('<button id="start-btn">Click here to get started</button>'); // Button element with text
-    $('body').append('<p id="count"></p>'); // Paragraph element with id='count'
-    $('body').append('<p>Copyright - Holberton School</p>'); // Another paragraph element
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button id="start-btn">Click here to get started</button>');
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyright - Holberton School</p>');
 });
