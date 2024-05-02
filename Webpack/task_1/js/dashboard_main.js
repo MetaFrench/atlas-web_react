66e0bdaf47d450ba29c66e178d1537e962a63c84
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-02 14:29:30 -0500

    main

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,9 +1,20 @@
+// src/index.js
 import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count">0</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 $(document).ready(function() {
-    $('body').append('<p>Holberton Dashboard</p>');
-    $('body').append('<p>Dashboard data for the students</p>');
-    $('body').append('<button id="start-btn">Click here to get started</button>');
-    $('body').append('<p id="count"></p>');
-    $('body').append('<p>Copyright - Holberton School</p>');
+  let count = 0;
+
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
+
+  $('button').on('click', _.debounce(updateCounter, 500));
 });
