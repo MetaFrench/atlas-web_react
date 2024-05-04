aaf639d24524804d79d94e13e0c50dbfa5d0b969
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-04 14:14:19 -0500

    files

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -1,12 +1,6 @@
-$(document).ready(function() {
-    // Function to add paragraphs to the page body
-    function addParagraphs() {
-        // Create and append three paragraphs with different text
-        $('body').append('<p>Holberton Dashboard</p>');
-        $('body').append('<p>Dashboard data for the students</p>');
-        $('body').append('<p>Copyright - Holberton School</p>');
-    }
+// src/index.js
+import $ from 'jquery';
 
-    // Call the function to add paragraphs
-    addParagraphs();
-});
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
