f5c1e4364b4881e4d2e37c8a24718d384559ca68
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-06 16:10:42 -0500

    js

--- react_intro/task_0/dashboard/src/App.js
+++ react_intro/task_0/dashboard/src/App.js
@@ -5,21 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+        </div>
       </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2024 - Holberton School</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
