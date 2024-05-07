7bb0e58edbf4ea97eee40d04ccab02a9a8228856
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-07 13:43:24 -0500

    js

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,30 @@
+import logo from './Holberton-Logo.png';
+import './App.css';
+import { getFooterCopy, getFullYear } from './utils'
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1>School Dashboard</h1>
+        </header>
+      </div>
+      <hr></hr>
+      <div>
+        <body className='App-body'>
+          <p className='login'>Login to access the full dashboard</p>
+        </body>
+      </div>
+      <hr></hr>
+      <div>
+        <footer className='App-footer'>
+          <p className='copyright'>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
