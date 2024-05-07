4a3bc704f617ff64e30a7b44cb7714a21c3a5868
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-07 13:33:54 -0500

    js

---
+++ react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Notifications.css'
+
+class Notification extends Component {
+  render() {
+    return (
+      <div className='Notifications'>
+        <p>Here is the list of notifications</p>
+      </div>
+    )
+  }
+}
+
+export default Notification
