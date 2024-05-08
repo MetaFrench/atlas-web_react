6c333fab0777ba7b2559c00f71e36d790ee91df0
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-08 12:05:50 -0500

    js

--- react_intro/task_1/dashboard/src/utils.js
+++ react_intro/task_1/dashboard/src/utils.js
@@ -1,3 +1,13 @@
 function getFullYear() {
-    return 2024
+  return 2024
+}
+
+function getFooterCopy(isIndex) {
+  if (isIndex === true) {
+    return 'Holberton School'
+  } else {
+    return 'Holberton School main dashboard'
   }
+}
+
+export { getFullYear, getFooterCopy };
