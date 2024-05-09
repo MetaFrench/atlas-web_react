7e3d2c3b2380b1ecf788eca32970202fd873ece3
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-08 23:05:28 -0500

    courselist task_5

--- react_props/task_5/dashboard/src/CourseList/CourseList.js
+++ react_props/task_5/dashboard/src/CourseList/CourseList.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import CourseListRow from './CourseListRow';
+import { CourseShape } from './CourseShape';
 import './CourseList.css'
 
-function CourseList() {
+function CourseList({ listCourses = [] }) {
   return (
     <>
       <table id='CourseList'>
@@ -11,14 +13,27 @@ function CourseList() {
           <CourseListRow  isHeader={true} textFirstCell='Course Name' textSecondCell='Credit' />
         </thead>
         <tbody>
-          <CourseListRow  isHeader={false} textFirstCell='ES6' textSecondCell='60' />
+          {listCourses.length === 0 ? (
+            <tr>
+              <td colSpan="2">No course available yet</td>
+            </tr>
+          ) : (
+            listCourses.map((course) => (
+              <CourseListRow key={course.id} isHeader={false} textFirstCell={course.name} textSecondCell={course.credit.toString()} />
+            ))
+          )}
+          {/* <CourseListRow  isHeader={false} textFirstCell='ES6' textSecondCell='60' />
           <CourseListRow  isHeader={false} textFirstCell='Webpack' textSecondCell='20' />
-          <CourseListRow  isHeader={false} textFirstCell='React' textSecondCell='40' />
+          <CourseListRow  isHeader={false} textFirstCell='React' textSecondCell='40' /> */}
         </tbody>
       </table>
       <hr></hr>
     </>
   );
 }
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape)
+};
 
 export default CourseList;
