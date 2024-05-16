c382b5e7530d09bc0b4dc03dc8853aecf2f477c8
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-16 13:34:44 -0500

    js_update

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,20 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Notification from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
 import Footer from '../Footer/Footer'
+import { getLatestNotification } from '../utils/utils';
 
-function App() {
-  return (
-    <>
-      <Notification />
-      <div className='App'>
-        <Header />
-        <Login />
-        <Footer />
-      </div>
-    </>
-  );
+class App extends React.Component {
+
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+  }
+
+  static defaultProps = {
+    isLoggedIn: false,
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ]
+  
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New Course Available'},
+      { id: 2, type: 'urgent', value: 'New Resume Available'},
+      { id: 3, html: { __html: getLatestNotification() }, type: 'urgent' }
+    ]
+
+    return (
+      <>
+        <Notification displayDrawer={true} listNotifications={listNotifications} />
+        <div className='App'>
+          <Header />
+          {isLoggedIn ? (
+              <CourseList listCourses={listCourses} />
+          ) : (
+              <Login />
+          )}
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
