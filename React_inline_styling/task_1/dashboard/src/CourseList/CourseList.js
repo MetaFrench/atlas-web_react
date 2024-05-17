6aa82aaa707ae831fde49d8a737bff095aab1af3
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-17 15:04:08 -0500

    React_inline_styling_task_1

--- React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 import CourseListRow from './CourseListRow';
 import { CourseShape } from './CourseShape';
-import './CourseList.css'
 
 function CourseList({ listCourses = [] }) {
   return (
     <>
-      <table id='CourseList'>
+      <table className={css(styles.courseList)} id='CourseList'>
         <thead>
           <CourseListRow  isHeader={true} textFirstCell='Available courses' />
           <CourseListRow  isHeader={true} textFirstCell='Course Name' textSecondCell='Credit' />
@@ -15,16 +15,13 @@ function CourseList({ listCourses = [] }) {
         <tbody>
           {listCourses.length === 0 ? (
             <tr>
-              <td colSpan="2">No course available yet</td>
+              <td className={css(styles.td)} colSpan="2">No course available yet</td>
             </tr>
           ) : (
             listCourses.map((course) => (
               <CourseListRow key={course.id} isHeader={false} textFirstCell={course.name} textSecondCell={course.credit.toString()} />
             ))
           )}
-          {/* <CourseListRow  isHeader={false} textFirstCell='ES6' textSecondCell='60' />
-          <CourseListRow  isHeader={false} textFirstCell='Webpack' textSecondCell='20' />
-          <CourseListRow  isHeader={false} textFirstCell='React' textSecondCell='40' /> */}
         </tbody>
       </table>
     </>
@@ -34,5 +31,36 @@ function CourseList({ listCourses = [] }) {
 CourseList.propTypes = {
   listCourses: PropTypes.arrayOf(CourseShape)
 };
+
+const styles = StyleSheet.create({
+  courseList: {
+    marginTop: '2rem',
+    border: '1px solid lightgray',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '90%',
+    borderCollapse: 'collapse'
+  },
+  
+  // topHeader: {
+  //   textAlign: 'center'
+  // },
+  
+  // table: {
+  //   marginLeft: 'auto',
+  //   marginRight: 'auto',
+  //   width: '90%',
+  //   borderCollapse: 'collapse'
+  // },
+  
+  // th: {
+  //   borderBottom: '2px solid lightgray'
+  // },
+  
+  td: {
+    padding: '3px',
+    textAlign: 'left'
+  }
+});
 
 export default CourseList;
