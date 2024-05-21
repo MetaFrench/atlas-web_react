9d8befa00ba545417776cdc5b5b8bdfb716ef42f
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-21 17:36:34 -0500

    react_state_app_update

--- react_state/task_2/dashboard/src/App/App.js
+++ react_state/task_2/dashboard/src/App/App.js
@@ -7,39 +7,48 @@ import Login from '../Login/Login';
 import BodySection from '../BodySection/BodySection';
 import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 import CourseList from '../CourseList/CourseList';
-import Footer from '../Footer/Footer'
+import Footer from '../Footer/Footer';
 import { getLatestNotification } from '../utils/utils';
+import AppContext from './AppContext';
 
 class App extends React.Component {
 
   static propTypes = {
-    isLoggedIn: PropTypes.bool,
+    // isLoggedIn: PropTypes.bool,
     logOut: PropTypes.func,
   }
 
   static defaultProps = {
-    isLoggedIn: false,
+    // isLoggedIn: false,
     logOut: () => {},
   }
 
   constructor(props) {
     super(props);
+    this.state = {
+      displayDrawer: false,
+      user: {
+        email: '',
+        password: '',
+        // isLoggedIn: false,
+      },
+      logOut: this.logOut,
+      logIn: this.logIn,
+    };
     this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
   }
 
   componentDidMount() {
-    // console.log("Adding keydown event listener");
     document.addEventListener('keydown', this.handleKeyDown);
   }
 
   componentWillUnmount() {
-    // console.log("Removing keydown event listener");
     document.removeEventListener('keydown', this.handleKeyDown);
   }
 
   handleKeyDown(event) {
-    // console.log("Key pressed: ", event.key);
-    // console.log("Ctrl key pressed: ", event.ctrlKey);
     if (event.ctrlKey && event.key === 'h') {
       event.preventDefault();
       alert('Logging you out');
@@ -47,8 +56,36 @@ class App extends React.Component {
     }
   }
 
+  handleDisplayDrawer () {
+    this.setState({ displayDrawer: true });
+  }
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  logIn = (email, password) => {
+    this.setState({
+      user: {
+        email,
+        password,
+        isLoggedIn: true,
+      }
+    });
+  }
+
+  logOut = () => {
+    this.setState({
+      user: {
+        email: '',
+        password: '',
+        isLoggedIn: false,
+      }
+    });
+  }
+
   render() {
-    const { isLoggedIn } = this.props;
+    // const { isLoggedIn } = this.props;
+    const { displayDrawer, user } = this.state;
 
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
@@ -63,28 +100,33 @@ class App extends React.Component {
     ]
 
     return (
-      <>
-        <Notification displayDrawer={true} listNotifications={listNotifications} />
+      <AppContext.Provider value={{ user: this.state.user, logOut: this.state.logOut, logIn: this.state.logIn }}>
+        <Notification
+          displayDrawer={this.state.displayDrawer}
+          handleDisplayDrawer={this.handleDisplayDrawer}
+          handleHideDrawer={this.handleHideDrawer}
+          listNotifications={listNotifications} 
+        />
         <div className='App'>
           <Header />
-          {isLoggedIn ? (
+          {user.isLoggedIn ? (
               <>
                 <BodySectionWithMarginBottom title="Course list">
                     <CourseList listCourses={listCourses} />
                 </BodySectionWithMarginBottom>
-                <BodySection title="News from the School">
-                    <p>Yee-haw!</p>
-                </BodySection>
               </>
           ) : (
               <BodySectionWithMarginBottom title="Log in to continue">
                   <Login />
               </BodySectionWithMarginBottom>
           )}
+          <BodySection title="News from the School">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </BodySection>
           <hr></hr>
           <Footer />
         </div>
-      </>
+      </ AppContext.Provider>
     );
   }
 }
