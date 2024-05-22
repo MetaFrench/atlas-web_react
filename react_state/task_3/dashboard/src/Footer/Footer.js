a9507ec266393678c53a2b9d69e9613dfc92828c
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-21 20:22:39 -0500

    react_state_app_footer_update

--- react_state/task_3/dashboard/src/Footer/Footer.js
+++ react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Footer.css';
 import { getFooterCopy, getFullYear } from '../utils/utils'
+import AppContext from '../App/AppContext'
 
 function Footer() {
+  const { user } = useContext(AppContext);
+
   return (
     <>
       <footer className='App-footer'>
+        {user.isLoggedIn && (<p className='contact-us'><a href='#'>Contact Us</a></p>)}
         <p className='copyright'>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
       </footer>
     </>
