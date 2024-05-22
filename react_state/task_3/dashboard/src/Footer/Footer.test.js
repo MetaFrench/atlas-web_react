a99c60b5352b47615e4bdb0701a9833d73d552cb
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-05-21 20:24:05 -0500

    react_state_app_footer_update

--- react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Footer from './Footer';
+import AppContext from '../App/AppContext';
 
 describe('Footer component tests', () => {
     let wrapper;
+    let contextValue;
 
     // This will run before each test and create a shallow render of the App component
     beforeEach(() => {
@@ -20,5 +22,37 @@ describe('Footer component tests', () => {
 
     test('Header renders an img element', () => {
       expect(wrapper.text()).toContain('Copyright');
+    });
+
+    test('verify that the contact link is not displayed when the user is logged out within the context', () => {
+      contextValue = {
+        user: {
+            email: 'test@example.com',
+            password: '',
+            isLoggedIn: false
+        },
+      };
+      wrapper = mount(
+          <AppContext.Provider value={contextValue}>
+              <Footer />
+          </AppContext.Provider>
+      );
+      expect(wrapper.find('.contact-us').exists()).toBe(false);
+    });
+
+    test('verify that the contact link is displayed when the user is logged in within the context', () => {
+      contextValue = {
+        user: {
+            email: 'test@example.com',
+            password: '',
+            isLoggedIn: true
+        },
+      };
+      wrapper = mount(
+          <AppContext.Provider value={contextValue}>
+              <Footer />
+          </AppContext.Provider>
+      );
+      expect(wrapper.find('.contact-us').exists()).toBe(true);
     });
 });
