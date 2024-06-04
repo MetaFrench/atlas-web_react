a307e3dced1e58a5536fbcc30721d7cb50e85c93
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-03 21:55:11 -0500

    react_redux_reducer_selector_task_1

--- react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,43 +1,25 @@
 import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT  } from '../actions/uiActionTypes'
-import { SELECT_COURSE } from '../actions/courseActionTypes';
+import { Map } from 'immutable'
 
-const initialState = {
+const initialState = Map({
   isNotificationDrawerVisible: false,
   isUserLoggedIn: false,
   user: {}
-}
+});
 
 export default function uiReducer(state = initialState, action) {
   switch (action.type) {
 
     case DISPLAY_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-        isNotificationDrawerVisible: true
-      };
-    
+      return state.set('isNotificationDrawerVisible', true);    
     case HIDE_NOTIFICATION_DRAWER:
-      return {
-        ...state,
-        isNotificationDrawerVisible: fase
-      };
-
+      return state .set('isNotificationDrawerVisible', false);
     case LOGIN_SUCCESS:
-      return {
-        ...state,
-        isUserLoggedIn: true
-      };
-
+      return state.set('isUserLoggedIn', true);
     case LOGIN_FAILURE:
-      return {
-        isUserLoggedIn: false
-      };
-
+      return state.set('isUserLoggedIn', false);
     case LOGOUT:
-      return {
-        isUserLoggedIn: false
-      };
-
+      return state.set('isUserLoggedIn', false);
     default:
       return state;
   }
