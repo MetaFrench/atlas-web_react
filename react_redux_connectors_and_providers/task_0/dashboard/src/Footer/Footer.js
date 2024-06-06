e1fbbd181732d21c0974bd701f06dae44066d578
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-05 21:32:20 -0500

    react_redux_connectors_and_providers_task_2

--- react_redux_connectors_and_providers/task_0/dashboard/src/Footer/Footer.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/Footer/Footer.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { connect } from 'react-redux';
 import './Footer.css';
 import { getFooterCopy, getFullYear } from '../utils/utils'
+// import AppContext from '../App/AppContext'
 
-function Footer() {
+function Footer({ user }) {
+  // const { user } = useContext(AppContext);
+
   return (
     <>
       <footer className='App-footer'>
+        {user.isLoggedIn && (<p className='contact-us'><a href='#'>Contact Us</a></p>)}
         <p className='copyright'>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
       </footer>
     </>
   );
 }
 
-export default Footer;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(mapStateToProps)(Footer);
+// export default Footer;
