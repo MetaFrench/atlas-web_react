b2d6f6fb3b122a68932bdafcc0e31616638606dc
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-05 19:17:49 -0500

    react_redux_connectors_and_providers_task_0

--- react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
+++ react_redux_connectors_and_providers/task_0/dashboard/src/App/App.test.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import { StyleSheetTestUtils } from 'aphrodite';
-import App from './App';
+import App, {mapStateToProps} from './App';
 import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import CourseList from '../CourseList/CourseList';
 import Footer from '../Footer/Footer';
+import AppContext from './AppContext';
+import { fromJS } from 'immutable';
 
 describe('App component tests', () => {
     let wrapper;
 
-    // This will run before each test and create a shallow render of the App component
     beforeEach(() => {
         wrapper = shallow(<App />);
         StyleSheetTestUtils.suppressStyleInjection();
+        // global.alert = jest.fn();
     });
 
     afterEach(() => {
@@ -42,24 +44,47 @@ describe('App component tests', () => {
     });
 
     test('CourseList is not displayed when isLoggedIn is false', () => {
-        wrapper = shallow(<App isLoggedIn={false} />);
+        // wrapper = shallow(<App isLoggedIn={false} />);
         expect(wrapper.find(CourseList).exists()).toBe(false);
     });
 
     test('Login is not displayed when isLoggedIn is true', () => {
-        wrapper = shallow(<App isLoggedIn={true} />);
+        // wrapper = shallow(<App isLoggedIn={true} />);
+        wrapper.setState({ 
+            user: { 
+                email: 'test@example.com',
+                password: '123', isLoggedIn: true 
+            } 
+        });
         expect(wrapper.find(Login).exists()).toBe(false);
     });
 
     test('CourseList is displayed when isLoggedIn is true', () => {
-        wrapper = shallow(<App isLoggedIn={true} />);
+        // wrapper = shallow(<App isLoggedIn={true} />);
+        wrapper.setState({ 
+            user: { 
+                email: 'test@example.com',
+                password: '123', isLoggedIn: true 
+            } 
+        });
         expect(wrapper.find(CourseList).exists()).toBe(true);
     });
 
     test('verify that when the keys control and h are pressed the logOut function, passed as a prop, is called and the alert function is called with the string Logging you out', () => {
-        const logOutMock = jest.fn();
-        const wrapper = shallow(<App logOut={logOutMock} />);
-        
+        const logOutMock = jest.fn(() => {
+            wrapper.instance().logOut();
+        });
+        // const wrapper = shallow(<App logOut={logOutMock} />);
+        wrapper.setState({ 
+            user: { 
+                email: 'test@example.com',
+                password: '123', isLoggedIn: true 
+            } 
+        });
+        wrapper.setProps({
+            logOut: logOutMock
+        });
+
         const event = {
             key: 'h',
             ctrlKey: true,
@@ -70,5 +95,66 @@ describe('App component tests', () => {
     
         expect(global.alert).toHaveBeenCalledWith('Logging you out');
         expect(logOutMock).toHaveBeenCalled();
+        // console.log(wrapper.state().user.isLoggedIn);
+        expect(wrapper.state().user.isLoggedIn).toBe(false);
+    });
+
+    test('verify that the default state for displayDrawer is false, then true after calling handleDisplayDrawer', () => {
+        expect(wrapper.state('displayDrawer')).toBe(false);
+        wrapper.instance().handleDisplayDrawer();
+        expect(wrapper.state('displayDrawer')).toBe(true);
+    });
+
+    test('verify that after calling handleHideDrawer, the state is updated to be false', () => {
+        wrapper.instance().handleDisplayDrawer();
+        expect(wrapper.state('displayDrawer')).toBe(true);
+        wrapper.instance().handleHideDrawer();
+        expect(wrapper.state('displayDrawer')).toBe(false);
+    });
+
+    test('verify that the logIn function updates the state correctly', () => {
+        wrapper.instance().logIn('test@example.com', 'password');
+        expect(wrapper.state().user).toEqual({ email: 'test@example.com', password: 'password', isLoggedIn: true });
+    });
+
+    test('verify that the logOut function updates the state correctly', () => {
+        wrapper.setState({ user: { email: 'test@example.com', password: '123', isLoggedIn: true } });
+        wrapper.instance().logOut();
+        expect(wrapper.state().user).toEqual({ email: '', password: '', isLoggedIn: false });
+    });
+
+    test('verify that markNotificationAsRead works as intended', () => {
+        wrapper.setState({
+            user: {
+                email: 'test@example.com',
+                password: '123',
+                isLoggedIn: true,
+            },
+            listNotifications: [
+                { id: 1, type: 'default', value: 'notification 1'},
+                { id: 2, type: 'urgent', value: 'notification 2'},
+                { id: 3, type: 'default', value: 'notification 3' },
+                { id: 4, type: 'urgent', value: 'notification 4' },
+            ],
+        });
+        // console.log(wrapper.state('listNotifications'));
+        expect(wrapper.state('listNotifications').length).toBe(4);
+        wrapper.instance().markNotificationAsRead(2);
+        expect(wrapper.state('listNotifications').length).toBe(3);
+        expect(wrapper.state('listNotifications')).toEqual([
+            { id: 1, type: 'default', value: 'notification 1'},
+            { id: 3, type: 'default', value: 'notification 3' },
+            { id: 4, type: 'urgent', value: 'notification 4' }
+        ]);
+    });
+});
+
+describe('mapStateToProps tests', () => {
+    test('should map the state correctly', () => {
+        const mockState = fromJS({
+          isUserLoggedIn: true
+        });
+        const state = mapStateToProps(mockState);
+        expect(state.isLoggedIn).toBe(true);
     });
 });
