6d4b601e95be2a664d4a0e39396c8ffcc6c50f4b
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-05 21:47:00 -0500

    react_redux_connectors_and_providers_task_2

--- react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.test.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.test.js
@@ -1,12 +1,11 @@
-import uiReducer from "./uiReducer";
-import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT  } from '../actions/uiActionTypes'
-// import { SELECT_COURSE } from "../actions/courseActionTypes";
+import uiReducer from './uiReducer';
+import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../actions/uiActionTypes';
 import { Map } from 'immutable';
 
 const initialState = Map({
   isNotificationDrawerVisible: false,
   isUserLoggedIn: false,
-  user: {}
+  user: null,
 });
 
 describe('ui reducer functions', () => {
@@ -16,15 +15,42 @@ describe('ui reducer functions', () => {
   });
 
   test('verifying the state returned by the uiReducer function equals the initial state when the action SELECT_COURSE is passed', () => {
-    const newState = uiReducer(undefined, { type: 'SELECT_COURSE' })
+    const newState = uiReducer(undefined, { type: 'SELECT_COURSE' });
     expect(newState.toJS()).toEqual(initialState.toJS());
   });
 
-  test('verifying the state returned, when DISPLAY_NOTIFICATION_DRAWER is passed, changes  the isNotificationDrawerVisible property', () => {
-    const newState = uiReducer(undefined, { type: 'DISPLAY_NOTIFICATION_DRAWER' })
+  test('verifying the state returned, when DISPLAY_NOTIFICATION_DRAWER is passed, changes the isNotificationDrawerVisible property', () => {
+    const newState = uiReducer(undefined, { type: DISPLAY_NOTIFICATION_DRAWER });
     expect(newState.toJS()).toEqual({
       ...initialState.toJS(),
-      isNotificationDrawerVisible: true
+      isNotificationDrawerVisible: true,
+    });
+  });
+
+  test('verifying the state returned, when HIDE_NOTIFICATION_DRAWER is passed, changes the isNotificationDrawerVisible property', () => {
+    const newState = uiReducer(undefined, { type: HIDE_NOTIFICATION_DRAWER });
+    expect(newState.toJS()).toEqual({
+      ...initialState.toJS(),
+      isNotificationDrawerVisible: false,
+    });
+  });
+
+  test('verifying the state returned, when LOGIN_SUCCESS is passed, changes the isUserLoggedIn property and sets the user', () => {
+    const user = { email: 'test@example.com' };
+    const newState = uiReducer(undefined, { type: LOGIN_SUCCESS, user });
+    expect(newState.toJS()).toEqual({
+      ...initialState.toJS(),
+      isUserLoggedIn: true,
+      user: user,
+    });
+  });
+
+  test('verifying the state returned, when LOGOUT is passed, changes the isUserLoggedIn property and sets the user to null', () => {
+    const newState = uiReducer(undefined, { type: LOGOUT });
+    expect(newState.toJS()).toEqual({
+      ...initialState.toJS(),
+      isUserLoggedIn: false,
+      user: null,
     });
   });
 });
