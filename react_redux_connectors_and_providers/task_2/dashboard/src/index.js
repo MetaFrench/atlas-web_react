eb48ead3dee1236e2e61ab324d9803f6e903293d
MetaFrench <98486522+MetaFrench@users.noreply.github.com>
2024-06-05 21:23:50 -0500

    react_redux_connectors_and_providers_task_2

--- react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import uiReducer from './reducers/uiReducer'; 
+import uiReducer from './reducers/uiReducer';
+// import thunk from 'redux-thunk';
+import { loginRequest } from './actions/uiActionCreators';
 
-
-const store = createStore(uiReducer);
+const store = configureStore({
+  reducer: {
+    ui: uiReducer
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: loginRequest
+      }
+    })
+});
+// export const store = createStore(uiReducer, applyMiddleware(thunk));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,3 +28,5 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+export default store;
